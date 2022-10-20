3a1c63ffb828231c550c01442839249a8b33024d
Rafael Petrachini <raffp@me.com>
2022-10-20 15:27:14 -0700

    Add the API call and separate logic

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-
-
+export default class Currency {
+  static getCurrency() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    .then((response) => response.json())
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Currency from './app.js';
 
 
+const inputOne = document.getElementById('currency-one');
+const inputAmount = document.getElementById('amount-one');
+const resultDiv = document.getElementById('result');
+const button = document.getElementById('submit');
 
   function getCurrency(currency) {
-    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then((response) => response.json())
-    .then(console.log);
+    const userInput = inputOne.value; 
+    const amount = inputAmount.value;
+
+      Currency.getCurrency(currency)
+    .then((data) => {
+      // console.log(data);
+      const rate = data.conversion_rates[userInput];
+      resultDiv.innerText = `1 usd = ${amount * rate}`
+    });
     }
 
-    getCurrency();
+
+    button.addEventListener('click', getCurrency)
+
+
