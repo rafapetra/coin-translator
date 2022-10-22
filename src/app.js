dacf0c5dc244eb07c04d97f3581b1702ab69a7d9
Rafael Petrachini <raffp@me.com>
2022-10-22 00:08:34 -0700

    Fix indentation on index.html and .js files

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
 export default class Currency {
   static getCurrency() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(data) {
-      if (data.status != 200) {
-        const errorMessage = `${data.status} ${data.statusText}`;
-        throw new Error(errorMessage);
-  
-
-      } else {
-        console.log(data)
-        return data.json();
-
-      }
-    })      
-    .catch(function(error) {
-      return error;
-    });
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+    )
+      .then(function (data) {
+        if (data.status != 200) {
+          const errorMessage = `${data.status} ${data.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          console.log(data);
+          return data.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
-// User Interface Logic 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import Currency from './app.js';
+// User Interface Logic
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import Currency from "./app.js";
 
-window.addEventListener("load", function() {
-  const button = document.getElementById('submit');
-  button.addEventListener('click', getCurrency)
+window.addEventListener("load", function () {
+  const button = document.getElementById("submit");
+  button.addEventListener("click", getCurrency);
 });
 
-  function getCurrency() {
-      Currency.getCurrency()
-    .then(function(data) {
-console.log(data)
-      if (data.result != 'success') {
-        printError(data);
+function getCurrency() {
+  Currency.getCurrency().then(function (data) {
+    if (data.result != "success") {
+      printError(data);
     } else {
       printElements(data);
     }
   });
 }
 
-    function printElements(data) {
-      const inputOne = document.getElementById('currency-one');
-      const inputAmount = document.getElementById('amount-one');
-      const resultDiv1 = document.getElementById('result1');
-      const resultDiv2 = document.getElementById('result2');
-      const equalSign = document.getElementById('equalSign');
-      const userInput = inputOne.value; 
-      const amount = inputAmount.value;
-      const rate = data.conversion_rates[userInput];
-      resultDiv1.innerText = `${amount} USD`;
-      equalSign.innerText = `=`;
-      resultDiv2.style.display = 'block';
-      resultDiv2.innerText = `${amount * rate} ${userInput}`;
-    }
+function printElements(data) {
+  const inputOne = document.getElementById("currency-one");
+  const inputAmount = document.getElementById("amount-one");
+  const resultDiv1 = document.getElementById("result1");
+  const resultDiv2 = document.getElementById("result2");
+  const equalSign = document.getElementById("equalSign");
+  const userInput = inputOne.value;
+  const amount = inputAmount.value;
+  const rate = data.conversion_rates[userInput];
+  resultDiv1.innerText = `${amount} USD`;
+  equalSign.innerText = `=`;
+  resultDiv2.style.display = "block";
+  resultDiv2.innerText = `${amount * rate} ${userInput}`;
+}
 
-    function printError(error) {
-      const inputOne = document.getElementById('currency-one').value;
-      const resultDiv1 = document.getElementById('result1');
-      resultDiv1.innerText = `There was an error accessing the currency data for ${inputOne}: ${error}`;
-    }
+function printError(error) {
+  const inputOne = document.getElementById("currency-one").value;
+  const resultDiv1 = document.getElementById("result1");
+  resultDiv1.innerText = `There was an error accessing the currency data for ${inputOne}: ${error}`;
+}
